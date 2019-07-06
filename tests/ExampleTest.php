1352e4d521e9e2a01de7e77986e9eacb7d1d0ceb
Mihir Parikh <mihir1408@gmail.com>
2019-07-06 10:08:23 +1000

    A simple example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+// This is a Test class
+class ExampleTest extends PHPUnit\Framework\TestCase {
+    // This is an individual test
+    public function testTwoPlusTwoEqualsFour() {
+        $this->assertEquals(4, 2 + 2);
+    }
+}
