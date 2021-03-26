82c74b73c49fb64702b170302873dd8fd4a6422b
Mihir Parikh <mparikh@genixventures.com>
2021-03-26 16:52:17 +1100

    A simple example of mocking using Mockery framework

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -2,6 +2,15 @@
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase {
+    /**
+     * Integrate Mockery framework with PHPUnit
+     * 
+     * tearDown() method is called after each test is run
+     */
+    public function tearDown(): void {
+        Mockery::close();
+    }
+
     public function testThatOrderIsProcessed() {
         $gateway = $this->getMockBuilder('PaymentGateway')
         ->setMethods(array('charge'))
@@ -11,6 +20,19 @@ class OrderTest extends TestCase {
         $order = new Order($gateway);
         $order->amount = 200;
 
+        $this->assertTrue($order->process());
+    }
+
+    public function testThatOrderIsProcessedUsingMockery() {
+        $gateway = Mockery::mock('PaymentGateway');
+
+        $gateway->shouldReceive('charge')
+                ->once()
+                ->with(200)
+                ->andReturn(true);
+        
+        $order = new Order($gateway);
+        $order->amount = 200;
         $this->assertTrue($order->process());
     }
 }
